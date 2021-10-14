cca96e18c101db560590203f813357199968d2cb
Niall <niall.bambury@codurance.com>
2021-10-14 11:56:16 +0100

    [NB] Add logic to prepopulate data from an API for the my details page

--- src/pages/myDetails/MyDetails.js
+++ src/pages/myDetails/MyDetails.js
@@ -1,38 +1,60 @@
-import { Label, LabelText, Input, Fieldset, DateField } from 'govuk-react'
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import { Label, LabelText, Input, Fieldset } from 'govuk-react'
+
 
 function MyDetails() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [age, setAge] = useState("");
+
+
+    useEffect(() => {
+        const getSubjectDetails = async () => {
+            const res = await axios.get(`http://localhost:3004/subject`);
+        
+            if(res){
+                const {firstName, lastName, age} =res.data.subject;
+            
+                setFirstName(firstName);
+                setLastName(lastName);
+                setAge(age);
+            }   
+        }
+
+        getSubjectDetails();
+    }, []);
+    
     return (
         <div>
             <h1>Your Details</h1>
 
             <Fieldset>
                 <Fieldset.Legend>Please enter your details</Fieldset.Legend>
-                <div class="form-group">
+                <div className="form-group">
                     <Label>
                         <LabelText>
                             First Name
                         </LabelText>
-                        <Input name="firstName" />
+                        <Input name="firstName" defaultValue={firstName}/>
                     </Label>
                 </div>
 
-                <div class="form-group">
+                <div className="form-group">
                     <Label>
                         <LabelText>
                             Last Name
                         </LabelText>
-                        <Input name="lastName" />
+                        <Input name="lastName" defaultValue={lastName} />
                     </Label>
                 </div>
 
-                <div class="form-group">
+                <div className="form-group">
                     <Label>
                         <LabelText>
                             Age
                         </LabelText>
-                        <DateField name="dateOfBirth" hintText="For example, 31 03 1980">
-                            What is your date of birth?
-                        </DateField>
+                        <Input name="age" defaultValue={age} />
                     </Label>
                 </div>
 
--- src/pages/myDetails/MyDetails.test.js
+++ src/pages/myDetails/MyDetails.test.js
@@ -1,16 +1,44 @@
 import { MyDetails } from './MyDetails'
 import { render, screen } from '@testing-library/react';
+import * as axios from 'axios';
+import { act } from 'react-dom/test-utils';
+
+jest.mock('axios');
+
+
+const mockSubject = {
+    subject: { firstName: "John", lastName: "Doe", age: 26}
+};
+const mockFetchSubject = results => {
+    axios.get.mockImplementation(() => Promise.resolve({
+        data: results
+    }));
+}
 
 describe('MyDetails', () => {
+
     const renderPage = async () => render(<MyDetails />);
 
     it('renders static content on the page', async () => {
         await renderPage();
 
         [
-            'My Details'
+            'Your Details',
+            'Please enter your details'
         ].forEach(content => {
             expect(screen.getByText(content)).toBeInTheDocument();
         });
+    });
+
+    it('Prepopulates input fields with data if we already have subject data',async ()=>{
+        mockFetchSubject(mockSubject);
+        
+        await act(async () => {
+            await renderPage();
+        });
+        
+        expect(screen.getByLabelText('First Name')).toHaveValue('John');
+        expect(screen.getByLabelText('Last Name')).toHaveValue('Doe');
+        expect(screen.getByLabelText('Age')).toHaveValue('26');
     })
 })
